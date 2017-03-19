eefcf9a307acd108f9655433623e3fe11695c790
NDESARAJ <desaraju02@gmail.com>
2017-03-19 21:14:24 +0530

    First commit on the Test2 project

---
+++ src/com/nag/Primer.java
@@ -0,0 +1,43 @@
+package com.nag;
+
+public class Primer {
+
+	public static void main(String[] args) {
+		
+		
+		
+		// TODO Auto-generated method stub
+		
+	int x =16-1*0+2/2;
+		
+		//System.out.println(x);
+		
+		System.out.println("The answer is: "+x);
+		System.out.println("And isEven is "+ isEven(x));
+		System.out.println("isPrime: "+isPrime(x));
+
+	}
+	
+	
+	
+
+public static boolean isEven(int x1)
+{
+	return (x1%2 == 0);
+}
+
+public static boolean isPrime(int x1)
+{
+	boolean isPrime = true;
+	for(int i = 2; i < x1/2; i++)
+	{
+		if (x1%i == 0) 
+			isPrime = false;
+			break;
+	}
+	
+	return isPrime;
+}
+
+
+}
